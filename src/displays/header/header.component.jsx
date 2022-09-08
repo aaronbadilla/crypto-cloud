4eaf0a9b23dc62b650fb83b9e1ad28f3d25e5385
Aaron Badilla <aaronbadilla@Aarons-MBP.askey.com>
2022-09-08 16:02:14 -0400

    tweaked mobile styles, fixed an ios chrome search focus issue

--- src/displays/header/header.component.jsx
+++ src/displays/header/header.component.jsx
@@ -39,7 +39,6 @@ const Header = ({ ...otherProps }) => {
 				) : null}
 				<MobileMenuIcon handleClick={handleMenuToggle} />
 				<div className="utilities-container">
-					<SearchBox placeholder="Search" />
 					<MenuButton
 						onClickHandler={sortByDailyVolume}
 						aria="Sort By Volume"
@@ -55,6 +54,7 @@ const Header = ({ ...otherProps }) => {
 						text="Favorites"
 						aria-label="Go To Favorites"
 					/>
+					<SearchBox placeholder="Search" />
 				</div>
 			</>
 		</div>
